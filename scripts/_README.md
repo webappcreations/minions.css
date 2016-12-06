916b840f56278c2a493fdea5e739b653da839565
Michael Chan <mijoch@gmail.com>
2016-12-06 13:38:23 -0800

    * generate READMEs for packages

---
+++ scripts/_README.md
@@ -0,0 +1,30 @@
+# minions.<%= packageName %>
+<%= mod.description %>
+
+## Module
+Latest: `<%= mod.version %>`
+main/style: `<%= mod.style %>`
+
+## Installation
+npm:
+```bash
+npm install minions.<%= packageName %>
+```
+
+browser:
+```html
+<link rel="stylesheet" href="https://unpkg.com/minions.align-content" />
+```
+
+## Code
+```css
+<%= code %>
+```
+
+## Source and issues
+<%= mod.repository %>/tree/master/packages/<%= packageName %>
+
+## License
+
+The MIT License (MIT)
+Copyright (c) 2016 Michael Chan
--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,14 +1,11 @@
 const fs = require("fs")
 const path = require("path")
-
-const getDirectories = (srcpath) =>
-  fs.readdirSync(srcpath).filter(file =>
-    fs.statSync(path.join(srcpath, file)).isDirectory())
-
-//console.log(getDirectories("./packages"))
+const cons = require("consolidate")
 
 fs.readdir("./packages", (_, packages) =>
-  packages.forEach(package =>
-    fs.readFile(`./packages/${package}/${package}.css`, "utf8", (_, file) =>
-      fs.writeFile(`./packages/${package}/README.md`, file, (_, file) =>
-        console.log(`${package} README.md written.\n`)))))
+  packages.forEach(packageName =>
+    fs.readFile(`./packages/${packageName}/package.json`, "utf8", (_, mod) =>
+      fs.readFile(`./packages/${packageName}/${packageName}.css`, "utf8", (_, file) =>
+        cons["lodash"]("scripts/_README.md", {package: packageName, code: file, mod: JSON.parse(mod)}, (_, md) =>
+          fs.writeFile(`./packages/${packageName}/README.md`, md, (_, _file) =>
+            console.log(`${packageName} README.md written.\n`)))))))
