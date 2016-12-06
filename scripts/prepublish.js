0fdc7a093ca80f94fbd65680e69e211d78365f49
Michael Chan <mijoch@gmail.com>
2016-12-06 13:04:58 -0800

    * get all the files

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -5,4 +5,9 @@ const getDirectories = (srcpath) =>
   fs.readdirSync(srcpath).filter(file =>
     fs.statSync(path.join(srcpath, file)).isDirectory())
 
-console.log(getDirectories("./packages"))
+//console.log(getDirectories("./packages"))
+
+fs.readdir("./packages", (_, packages) =>
+  packages.forEach(package =>
+    fs.readFile(`./packages/${package}/${package}.css`, "utf8", (_, file) =>
+      console.log(package, file))))
