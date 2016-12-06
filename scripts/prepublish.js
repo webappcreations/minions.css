e83eabf189c3991e7d78ccdedf563cfec844ee67
Michael Chan <mijoch@gmail.com>
2016-12-06 14:32:06 -0800

    * generate html package html index files

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -2,10 +2,18 @@ const fs = require("fs")
 const path = require("path")
 const cons = require("consolidate")
 
+const writeFiles = ({ packageName, code, mod }) => {
+  cons["lodash"]("scripts/_README.md", {packageName, code, mod}, (_, md) =>
+    fs.writeFile(`./packages/${packageName}/README.md`, md, (_, __) =>
+      console.log(`${packageName} README.md written.`)))
+
+  cons["lodash"]("scripts/_index.html", {packageName, code, mod}, (_, md) =>
+    fs.writeFile(`./packages/${packageName}/index.html`, md, (_, __) =>
+      console.log(`${packageName} index.html written.`)))
+}
+
 fs.readdir("./packages", (_, packages) =>
   packages.forEach(packageName =>
     fs.readFile(`./packages/${packageName}/package.json`, "utf8", (_, mod) =>
-      fs.readFile(`./packages/${packageName}/${packageName}.css`, "utf8", (_, file) =>
-        cons["lodash"]("scripts/_README.md", {packageName: packageName, code: file, mod: JSON.parse(mod)}, (_, md) =>
-          fs.writeFile(`./packages/${packageName}/README.md`, md, (_, _file) =>
-            console.log(`${packageName} README.md written.\n`)))))))
+      fs.readFile(`./packages/${packageName}/${packageName}.css`, "utf8", (_, code) =>
+        writeFiles({packageName, code, mod: JSON.parse(mod)})))))
