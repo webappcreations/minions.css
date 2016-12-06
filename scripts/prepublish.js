8be6b2fd2fbc38fe455d84d6945656e2bddc0686
Michael Chan <mijoch@gmail.com>
2016-12-06 14:56:14 -0800

    * add a root-level index

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -7,8 +7,8 @@ const writeFiles = ({ packageName, code, mod }) => {
     fs.writeFile(`./packages/${packageName}/README.md`, md, (_, __) =>
       console.log(`${packageName} README.md written.`)))
 
-  cons["lodash"]("scripts/_index.html", {packageName, code, mod}, (_, md) =>
-    fs.writeFile(`./packages/${packageName}/index.html`, md, (_, __) =>
+  cons["lodash"]("scripts/_index.html", {packageName, code, mod}, (_, html) =>
+    fs.writeFile(`./packages/${packageName}/index.html`, html, (_, __) =>
       console.log(`${packageName} index.html written.`)))
 }
 
@@ -17,3 +17,7 @@ fs.readdir("./packages", (_, packages) =>
     fs.readFile(`./packages/${packageName}/package.json`, "utf8", (_, mod) =>
       fs.readFile(`./packages/${packageName}/${packageName}.css`, "utf8", (_, code) =>
         writeFiles({packageName, code, mod: JSON.parse(mod)})))))
+
+fs.readdir("./packages", (_, packages) =>
+  cons["lodash"]("scripts/_package_index.html", {packages}, (_, html) =>
+    fs.writeFile(`./index.html`, html, (_, __) => console.log(`index.html written.`))))
